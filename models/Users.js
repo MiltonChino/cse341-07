9a2327efd8f32e177ce4c5f4c6b73f12a249ae6e
Milton Chino <miltonchinowrk@gmail.com>
2024-11-25 09:28:33 -0400

    updating models for Google oauth

--- models/Users.js
+++ models/Users.js
@@ -1,28 +1,31 @@
 const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
 
-const userSchema = mongoose.Schema(
-  {
-    user: {
-      firstName: {
-        type: String,
-        required: true,
-      },
-      lastName: {
-        type: String,
-        required: true,
-      },
-      email: {
-        type: String,
-        required: true,
-        lowercase: true,
-      },
-      level: {
-        type: Number,
-        required: true,
-      },
-    },
-  },
-  { timestamps: true }
-);
+// const userSchema = mongoose.Schema(
+//   {
+//     user: {
+//       firstName: {
+//         type: String,
+//         required: true,
+//       },
+//       lastName: {
+//         type: String,
+//         required: true,
+//       },
+//       email: {
+//         type: String,
+//         required: true,
+//         lowercase: true,
+//       },
+//       level: {
+//         type: Number,
+//         required: true,
+//       },
+//     },
+//   },
+//   { timestamps: true }
+// );
+
+const userSchema = mongoose.Schema();
 
 module.exports = mongoose.model("users", userSchema);
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const passport = require("./config/passport-setup");
 const axios = require("axios");
 const connectDB = require("./connection/db");
+const User = require("../models/users");
 
 const app = express();
 const PORT = 3000 || process.env.PORT;
@@ -14,6 +15,39 @@ connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/", require("./routes"));
+
+// Google OAuth setup
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: `${process.env.BASE_URL}/auth/google/callback`, // Ensure BASE_URL is defined in your .env file
+    },
+    async function (accessToken, refreshToken, profile, done) {
+      let user = await User.findOrCreate(
+        { googleId: profile.id },
+        function (error, user) {
+          return done(error, user);
+        }
+      );
+    }
+  )
+);
+
+// Serialize and deserialize user for session management
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
 
 // Session middleware configuration
 app.use(
